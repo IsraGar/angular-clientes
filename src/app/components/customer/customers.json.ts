13dcb5244df931812793badc8c1d66dbf69d059b
IsraGar <isragarciahdez@gmail.com>
2024-01-23 11:56:41 -0600

    Cambio de tipo de dato en clase Customer+

--- src/app/components/customer/customers.json.ts
+++ src/app/components/customer/customers.json.ts
@@ -1,14 +1,14 @@
 import { Customer } from "../entity/customer";
 
 export const CUSTOMERS: Customer[] = [
-    { id: '001', name: 'Israel', lastname: 'Garcia', createdAt: '2024-01-22', email: 'imgarcia@mail.com' },
-    { id: '002', name: 'Juan', lastname: 'Gomez', createdAt: '2024-01-22', email: 'jugomez@mail.com' },
-    { id: '003', name: 'Roberto', lastname: 'Lopez', createdAt: '2024-01-23', email: 'rolopez@mail.com' },
-    { id: '004', name: 'Maria', lastname: 'Escamilla', createdAt: '2024-01-23', email: 'maescamilla@mail.com' },
-    { id: '005', name: 'Daniel', lastname: 'Sanchez', createdAt: '2024-01-24', email: 'dasanchez@mail.com' },
-    { id: '006', name: 'Rosa', lastname: 'Hernandez', createdAt: '2024-01-24', email: 'rohernandez@mail.com' },
-    { id: '007', name: 'Marina', lastname: 'Fernandez', createdAt: '2024-01-25', email: 'mafernandez@mail.com' },
-    { id: '008', name: 'Karen', lastname: 'Rodriguez', createdAt: '2024-01-25', email: 'karodriguez@mail.com' },
-    { id: '009', name: 'Rodrigo', lastname: 'Martinez', createdAt: '2024-01-26', email: 'romartinez@mail.com' },
-    { id: '010', name: 'Samuel', lastname: 'Galicia', createdAt: '2024-01-27', email: 'sagalicia@mail.com' },
+    { id: 1, name: 'Israel', lastname: 'Garcia', createdAt: '2024-01-22', email: 'imgarcia@mail.com' },
+    { id: 2, name: 'Juan', lastname: 'Gomez', createdAt: '2024-01-22', email: 'jugomez@mail.com' },
+    { id: 3, name: 'Roberto', lastname: 'Lopez', createdAt: '2024-01-23', email: 'rolopez@mail.com' },
+    { id: 4, name: 'Maria', lastname: 'Escamilla', createdAt: '2024-01-23', email: 'maescamilla@mail.com' },
+    { id: 5, name: 'Daniel', lastname: 'Sanchez', createdAt: '2024-01-24', email: 'dasanchez@mail.com' },
+    { id: 6, name: 'Rosa', lastname: 'Hernandez', createdAt: '2024-01-24', email: 'rohernandez@mail.com' },
+    { id: 7, name: 'Marina', lastname: 'Fernandez', createdAt: '2024-01-25', email: 'mafernandez@mail.com' },
+    { id: 8, name: 'Karen', lastname: 'Rodriguez', createdAt: '2024-01-25', email: 'karodriguez@mail.com' },
+    { id: 9, name: 'Rodrigo', lastname: 'Martinez', createdAt: '2024-01-26', email: 'romartinez@mail.com' },
+    { id: 10, name: 'Samuel', lastname: 'Galicia', createdAt: '2024-01-27', email: 'sagalicia@mail.com' },
 ];
--- src/app/components/entity/customer.ts
+++ src/app/components/entity/customer.ts
@@ -1,5 +1,5 @@
 export class Customer{
-    constructor(id: string, name: string , lastname: string, createdAt: string, email: string){
+    constructor(id: number, name: string , lastname: string, createdAt: string, email: string){
         this.id = id;
         this.name = name;
         this.lastname = lastname;
@@ -7,7 +7,7 @@ export class Customer{
         this.email = email;
     }
 
-    id: string;
+    id: number;
     name: string;
     lastname: string;
     createdAt: string;
