a97ae2bc3ba6a170252c4860c10bf47b1d6458b9
IsraGar <isragarciahdez@gmail.com>
2024-01-24 13:57:41 -0600

    Manejo de errores desde el servicio

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Customer } from '../components/entity/customer';
-import { Observable, of } from 'rxjs';
+import { Observable, map, catchError, throwError } from 'rxjs';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +12,72 @@ export class CustomerService {
 
   private urlEndPoint: string = 'http://localhost:8080/api/customers/';
   private http = inject(HttpClient);
+  private router = inject(Router);
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   constructor() { }
 
   getCustomers(): Observable<Customer[]>{
     return this.http.get<Customer[]>(`${this.urlEndPoint}list`);
+  }  
+
+  getCustomer(id: number): Observable<Customer>{
+    return this.http.get<Customer>(`${this.urlEndPoint}list/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/customers']);
+        console.log(e.error.message);        
+        Swal.fire({
+          title: 'Invalid Id Customer',
+          text: e.error.message,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+        return throwError(e);
+      })
+    );
   }
 
   createCustomer(customer: Customer): Observable<Customer>{    
-    return this.http.post<Customer>(`${this.urlEndPoint}create`, customer, {headers: this.httpHeaders});
-  }
-
-  getCustomer(id: number): Observable<Customer>{
-    return this.http.get<Customer>(`${this.urlEndPoint}list/${id}`);
+    return this.http.post<Customer>(`${this.urlEndPoint}create`, customer, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.log(e.error.message);        
+        Swal.fire({
+          title: e.error.message,
+          text: e.error.error,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+        return throwError(e);
+      })
+    );
   }
 
   updateCustomer(customer: Customer): Observable<Customer>{
-    return this.http.put<Customer>(`${this.urlEndPoint}update/${customer.id}`, customer, {headers: this.httpHeaders});
+    return this.http.put<Customer>(`${this.urlEndPoint}update/${customer.id}`, customer, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.log(e.error.message);        
+        Swal.fire({
+          title: e.error.message,
+          text: e.error.error,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+        return throwError(e);
+      })
+    );
   }
 
   deleteCustomer(id: number): Observable<Customer>{
-    return this.http.delete<Customer>(`${this.urlEndPoint}delete/${id}`, {headers: this.httpHeaders})
+    return this.http.delete<Customer>(`${this.urlEndPoint}delete/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        console.log(e.error.message);        
+        Swal.fire({
+          title: e.error.message,
+          text: e.error.error,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+        return throwError(e);
+      })
+    );
   }
 }
